ea9c4dfbc036dc7ac5b360e8649c3ca6a5b6541d
shlomiya <yaakobovich.a@husky.neu.edu>
2017-11-16 12:15:02 -0500

    add usage

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-int main(int argc, char** argv)
-{
+using namespace std;
 
+inline void Usage(const char* cmd)
+{
+	cout << "Usage: " << cmd << " -m <sss>" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc <= 1) 
+		Usage(argv[1]);
 
 }
 
